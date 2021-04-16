76160e99a926288e279809e857f1ec8fc099ff69
Muhammad Bilal <bilal.se74@gmail.com>
2021-04-16 07:17:08 +0500

    Added sendData script for sending data to thing speak channel

---
+++ thingSpeak/Assets/sendData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class sendData : MonoBehaviour
+{
+    int randomTempValue;
+
+
+    void Start()
+    {
+        StartCoroutine(GetRequest("https://api.thingspeak.com/update?api_key=COZF3VWIZ0J02MAL&field1="));
+    }
+
+
+    private void Update()
+    {
+        
+
+        
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        randomTempValue = (int)Random.Range(0f, 100f);
+        UnityWebRequest uwr = UnityWebRequest.Get(uri+randomTempValue);
+        yield return uwr.SendWebRequest();
+        
+
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+        yield return new WaitForSeconds(10);
+        StartCoroutine(GetRequest("https://api.thingspeak.com/update?api_key=COZF3VWIZ0J02MAL&field1="));
+
+
+    }
+}
+
