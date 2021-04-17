9e424d52457c41f9b7423f50315f3b0b2e3e911d
Muhammad Bilal <bilal.se74@gmail.com>
2021-04-17 09:16:30 +0500

    add humidity varialble for sending data to thingspeak

--- thingSpeak/Assets/sendData.cs
+++ thingSpeak/Assets/sendData.cs
@@ -7,19 +7,35 @@ using UnityEngine.Networking;
 public class sendData : MonoBehaviour
 {
     int randomTempValue;
+    int randomHumdityValue;
 
 
     void Start()
     {
         StartCoroutine(GetRequest("https://api.thingspeak.com/update?api_key=COZF3VWIZ0J02MAL&field1="));
+        StartCoroutine(GetRequest2("https://api.thingspeak.com/update?api_key=COZF3VWIZ0J02MAL&field2="));
     }
 
-
-    private void Update()
+    IEnumerator GetRequest2(string uri)
     {
-        
+        randomHumdityValue = (int)Random.Range(0f, 100f);
+        UnityWebRequest uwr = UnityWebRequest.Get(uri + randomHumdityValue);
+        yield return uwr.SendWebRequest();
 
-        
+
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+        yield return new WaitForSeconds(10);
+        StartCoroutine(GetRequest2("https://api.thingspeak.com/update?api_key=COZF3VWIZ0J02MAL&field2="));
+
+
     }
 
     IEnumerator GetRequest(string uri)
